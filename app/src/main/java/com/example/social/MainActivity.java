5ae6d0a05711406ee5e491588ddb6411b2754984
sidhartha <sidhartha758013@gmail.com>
2020-11-20 20:53:50 +0530

    final release

---
+++ app/src/main/java/com/example/social/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.social;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+        setContentView(R.layout.activity_main);
+    }
+}
